c410463cd719fdb99968fbadad8a8aff4f2503d0
z397318716 <397318716@qq.com>
2019-11-09 11:28:20 +0800

    每日一题 Day04

---
+++ 19_11_9_OJ4/19_11_9_OJ4/test1.cpp
@@ -0,0 +1,55 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//
+//void CountSuger(vector<int> v1)
+//{
+//	vector<int> vtemp;
+//	int A = 0, B = 0;
+//	A = (v1[0] + v1[2]) / 2;
+//	B = (v1[1] + v1[3]) / 2;
+//	vtemp.push_back(A);
+//	vtemp.push_back(B);
+//	if ((v1[0] + v1[1]) != (v1[2] - v1[3]))
+//	{
+//		cout << "No";
+//		return;
+//	} else
+//	{
+//		vtemp.push_back(A - (v1[0] + v1[1]));
+//		for (int i = 0; i < vtemp.size(); i++)
+//		{
+//			if (i == vtemp.size() - 1)
+//			{
+//				cout << vtemp[i];
+//			}
+//			else
+//				cout << vtemp[i] << ' ';
+//		}
+//	}
+//
+//}
+//
+//
+//int main()
+//{
+//	//vector<int> v1;
+//	int arr[4] = { 0 };
+//	//getline(cin, v1);
+//	scanf("%d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3]);
+//	/*
+//	for(auto &i :arr)
+//	{
+//	if(i < -30 || i > 30)
+//	{
+//	return 0;
+//	}
+//	}
+//	*/
+//	vector<int> v1(arr, arr + 4);
+//	CountSuger(v1);
+//
+//	return 0;
+//}
---
+++ 19_11_9_OJ4/19_11_9_OJ4/test2.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+string Solution(int m, int n)
+{
+	string s1;
+	int tmp = 0;
+	char s[16] = { '0','1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+	while (m)
+	{
+		tmp = m % n;
+		m = m / n;
+		
+		s1.push_back(s[tmp]);
+	}
+
+	return s1;
+}
+
+
+
+int main()
+{
+	int m = 0, n = 0;
+	scanf("%ld %d", &m, &n);
+	if (n <= 0)
+	{
+		return 0;
+	}
+	string s1(Solution(m, n));
+
+	string::reverse_iterator it;
+	for (it = s1.rbegin(); it != s1.rend(); it++)
+	{
+		cout << *it;
+	}
+
+	return 0;
+}
