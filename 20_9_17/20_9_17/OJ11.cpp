e8065004446b9fd0593401e66c65de7f6129c09c
z397318716 <397318716@qq.com>
2020-09-17 22:35:30 +0800

    OJ练习

---
+++ 20_9_17/20_9_17/OJ11.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+class Solution {
+public:
+	int maxArea(vector<int>& height) {
+		int res = 0;
+		int i = 0;
+		int j = height.size() - 1;
+		while (i < j) {
+			int area = (j - i) * min(height[i], height[j]);
+			res = max(res, area);
+			if (height[i] < height[j]) {
+				i++;
+			} else {
+				j--;
+			}
+		}
+		return res;
+	}
+};
