473266694e7e7fbac7a01f91ddfa7ddb6e5db4c2
z397318716 <397318716@qq.com>
2020-10-09 09:57:02 +0800

    力扣练习

---
+++ 20_20_9/20_20_9/OJ26.cpp
@@ -0,0 +1,31 @@
+/************************************************************************/
+/* 给定一个排序数组，你需要在 原地 删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。                                                                     */
+/************************************************************************/
+#include<iostream>
+#include<vector>
+
+using namespace std;
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		int res = 0;
+		if (nums.size() < 2)
+			return nums.size();
+		for (int i = 1; i < nums.size(); ++i)
+		{
+			if (nums[i] == nums[res])
+			{
+				continue;
+			} else
+			{
+				//res++;
+				nums[++res] = nums[i];
+			}
+		}
+		return res + 1;
+	}
+};
