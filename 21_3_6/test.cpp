84bb00153e5d4d4532231431371c5e4083ab3fa4
z397318716 <397318716@qq.com>
2021-03-07 21:21:19 +0800

    练习

--- 21_3_6/test.cpp
+++ 21_3_6/test.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-class Solution{
-    void backTrack(vector<int> nums, vector<vector<int>>& res, vector<int>& tmp, int pos) {
-        res.push_back(tmp);
-        for (int i = pos; i < nums.size(); ++i) {
-            tmp.push_back(nums[i]);
-            backTrack(nums, res, tmp, i + 1);
-            tmp.pop_back();
-        }
-        return;
-    }
-    vector<vector<int>> subsets(vector<vector<int>>& nums) {
-        vector<vector<int>> res;
-        vector<int> tmp;
-        //res.push_back(tmp);
-        backTrack(nums, res, tmp, 0);
-        return res;
-    }
-};
-
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//class Solution{
+//    void backTrack(vector<vectorint>> nums, vector<vector<int>>& res, vector<int>& tmp, int pos) {
+//        res.push_back(tmp);
+//        for (int i = pos; i < nums.size(); ++i) {
+//            tmp.push_back(nums[i]);
+//            backTrack(nums, res, tmp, i + 1);
+//            tmp.pop_back();
+//        }
+//        return;
+//    }
+//    vector<vector<int>> subsets(vector<vector<int>>& nums) {
+//        vector<vector<int>> res;
+//        vector<int> tmp;
+//        //res.push_back(tmp);
+//        backTrack(nums, res, tmp, 0);
+//        return res;
+//    }
+//};
+//
