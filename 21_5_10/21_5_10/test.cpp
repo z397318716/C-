b9903bf0711b9fab8f8d05d28bca4ba74a0de458
z397318716 <397318716@qq.com>
2021-05-11 21:49:37 +0800

    练习

--- 21_5_10/21_5_10/test.cpp
+++ 21_5_10/21_5_10/test.cpp
@@ -30,4 +30,30 @@ public:
         }
         return res;
     }
+    vector<int> constructArr122(vector<int>& a) {
+        // ±©Á¦---³¬Ê±
+        /*
+        vector<int> res(a.size());
+        for(int i = 0; i < a.size(); ++i){
+            int tmp = 1;
+            for(int j = 0; j < a.size(); ++j){
+                if(j == i)
+                    continue;
+                tmp *= a[j];
+            }
+            res[i] = tmp;
+        }
+        return res;
+        */
+        vector<int> res(a.size(), 1);
+        int tmp = 1;
+        for (int i = 1; i < a.size(); ++i) {
+            res[i] = res[i - 1] * a[i - 1];
+        }
+        for (int i = a.size() - 2; i >= 0; --i) {
+            tmp *= a[i + 1];
+            res[i] *= tmp;
+        }
+        return res;
+    }
 };
