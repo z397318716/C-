436eb733a302ed8249f889d43882b7d50bfcb7e4
z397318716 <397318716@qq.com>
2021-06-08 14:49:23 +0800

    练习

--- 21_6_7/21_6_7/21_6_7.cpp
+++ 21_6_7/21_6_7/21_6_7.cpp
@@ -1,9 +1,56 @@
 ﻿// 21_6_7.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
-
-
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using std::vector;
+using std::string;
+using std::max;
+class Solution {
+    int count = 0;
+public:
+    vector<int> getZerosOnes(string& str) {
+        vector<int> zerosOnes(2);
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            zerosOnes[str[i] - '0']++;
+        }
+        return zerosOnes;
+    }
+
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int length = strs.size();
+        vector<vector<vector<int>>> dp(length + 1, vector<vector<int>>(m + 1, vector<int>(n + 1)));
+        for (int i = 1; i <= length; i++) {
+            vector<int>&& zerosOnes = getZerosOnes(strs[i - 1]);
+            int zeros = zerosOnes[0], ones = zerosOnes[1];
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k <= n; k++) {
+                    dp[i][j][k] = dp[i - 1][j][k];
+                    if (j >= zeros && k >= ones) {
+                        dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j - zeros][k - ones] + 1);
+                    }
+                }
+            }
+        }
+        return dp[length][m][n];
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        /* 回溯法 */
+        backtrack(nums, target, 0, 0);
+        return count;
+    }
+    void backtrack(vector<int>& nums, int target, int index, int sum) {
+        if (index == nums.size()) {
+            if (sum == target)
+                count++;
+            return;
+        }
+        backtrack(nums, target, index + 1, sum + nums[index]);
+        backtrack(nums, target, index + 1, sum - nums[index]);
+
+    }
+};
 
 int main()
 {
