8b608e3ee20be706a60ebbe9182eda7cca50ffd6
z397318716 <397318716@qq.com>
2021-06-22 10:37:48 +0800

    练习“

---
+++ 21_6_22/21_6_22/test.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    vector<string> res;
+    void dfs(string s, int pos) {
+        if (pos == s.size() - 1) {
+            res.push_back(s);
+            return;
+        }
+        set<char> st;
+        for (int i = pos; i < s.size(); ++i) {
+            if (st.find(s[i]) == st.end()) {
+                st.insert(s[i]);
+                swap(s[i], s[pos]);
+                dfs(s, pos + 1);
+                swap(s[i], s[pos]);
+            }
+        }
+    }
+public:
+    vector<string> permutation(string s) {
+        dfs(s, 0);
+        return res;
+    }
+};
