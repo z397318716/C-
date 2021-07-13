1881b5ea5fdc198547d6a82e3da42a81609d39f3
z397318716 <397318716@qq.com>
2021-07-13 22:23:01 +0800

    练习

---
+++ 21_7_13/21_7_13/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+//class Solution {
+//public:
+//    bool isFlipedString(string s1, string s2) {
+//        if (s1.size() != s2.size())
+//            return false;
+//        if (s1.empty())
+//            return true;
+//        string res;
+//        for (int i = 0; i < s1.size(); ++i) {
+//            if (s1[i] == s2[0]) {
+//                res.assign(s1.begin() + i, s1.end());
+//                res.append(s1.begin(), s1.begin() + i);
+//                if (res == s2)
+//                    return true;
+//            }
+//        }
+//
+//        return false;
+//    }
+//};
+
+int main() {
+
+	cout << (5 && 4) << endl;
+
+	return 0;
+}
