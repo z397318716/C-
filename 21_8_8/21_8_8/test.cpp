689a24724393aa9d86f34539dddc5624a3257cec
z397318716 <397318716@qq.com>
2021-08-09 20:28:46 +0800

    练习”

--- 21_8_8/21_8_8/test.cpp
+++ 21_8_8/21_8_8/test.cpp
@@ -1,3 +1,100 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+double Division(int a, int b)
+{
+    // 当b == 0时抛出异常
+    if (b == 0)
+        throw "Division by zero condition!";
+    else
+        return ((double)a / (double)b);
+}
+void Func()
+{
+    int len, time;
+    len = 10;
+    time = 0;
+    cout << Division(len, time) << endl;
+}
+void test() {
+    try {
+        Func();
+    }
+    catch (const char* errmsg) {
+        cout << errmsg << endl;
+    }
+    catch (...) {
+        cout << "unknown exception" << endl;
+    }
+}
+//class Mytest {
+//public:
+//    Mytest(const string& str)
+//        :_str(str) {}
+//    ~Mytest() {
+//        std::cout << _str << "destory" << std::endl;
+//    }
+//private:
+//    string _str;
+//};
+class Mytest:public enable_shared_from_this<Mytest> {
+public:
+    Mytest(const string& str)
+        :_str(str) {}
+    ~Mytest() {
+        std::cout << _str << "destory" << std::endl;
+    }
+    shared_ptr<Mytest> GetSharedptr() {
+        return shared_from_this();
+    }
+private:
+    string _str;
+};
+
+void delFun(string* p) {
+    std::cout << "Fun delete " << *p << endl;
+    delete p;
+}
+int main() {
+
+    std::cout << "begin" << std::endl;
+    shared_ptr<string> p1;
+    {
+        shared_ptr<string> p2(new string("p1"), [](string* p) {
+            std::cout << "Lamda delete " << *p << std::endl;
+            delete p;
+        });
+        p1 = p2;
+        shared_ptr<string> p3(new string("p3"), delFun);
+    }
+    std::cout << "end" << std::endl;
+    //test();
+   /* shared_ptr<int> ptr{ new int(3) };
+    shared_ptr<int> ptr1;
+    ptr1.reset(new int(3));
+    shared_ptr<int> ptr2 = make_shared<int>(3);*/
+    /*shared_ptr<int> p1 = make_shared<int>(1);
+    shared_ptr<int> p2 = make_shared<int>(2);
+    shared_ptr<int> p3;
+    shared_ptr<double> p4 = make_shared<double>(1);
+
+    bool b1 = p1 < p2;
+    bool b2 = p1 > p3;
+    bool b3 = p3 == p3;*/
+   // bool b4 = p4 < p2;
+
+    //Mytest* p = new Mytest("shared_test");
+    //shared_ptr<Mytest> p1(p);
+    //shared_ptr<Mytest> p2(p); // 
+    //Mytest* p = new Mytest("shared_test");
+    //shared_ptr<Mytest> p1(p);
+    //shared_ptr<Mytest> p2 = p->GetSharedptr(); // 正确做法
+    
+
+    return 0;
+}
+
+/*
 class Solution {
 public:
     int tribonacci(int n) {
@@ -16,3 +113,4 @@ public:
         return res[n];
     }
 };
+*/
