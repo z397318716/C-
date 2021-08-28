ad1cc164757558aeb4e1b5ebe58cf2a8222c4e84
z397318716 <397318716@qq.com>
2021-08-28 22:38:55 +0800

    练习

--- 21_8_27/21_8_27/quicksort.cpp
+++ 21_8_27/21_8_27/quicksort.cpp
@@ -53,6 +53,75 @@ public:
 	}
 };
 
+/**
+ * struct TreeNode {
+ *	int val;
+ *	struct TreeNode *left;
+ *	struct TreeNode *right;
+ * };
+ */
+class Solution2 {
+public:
+    /**
+     *
+     * @param root TreeNode类 the root of binary tree
+     * @return int整型vector<vector<>>
+     */
+    void prev(TreeNode* root, vector<int>& res) {
+        stack<TreeNode*> q1;
+        q1.push(root);
+        while (!q1.empty()) {
+            TreeNode* tmp = q1.top();
+            q1.pop();
+            res.push_back(tmp->val);
+            if (tmp->right)
+                q1.push(tmp->right);
+            if (tmp->left)
+                q1.push(tmp->left);
+        }
+    }
+    void mid(TreeNode* root, vector<int>& res) {
+        stack<TreeNode*> q1;
+        while (root || !q1.empty()) {
+            while (root) {
+                q1.push(root);
+                root = root->left;
+            }
+            root = q1.top();
+            q1.pop();
+            res.push_back(root->val);
+            root = root->right;
+        }
+    }
+    void post(TreeNode* root, vector<int>& res) {
+        if (root == nullptr)
+            return;
+        // 与前序类似遍历方法
+        // 根右左  最后在逆转
+        stack<TreeNode*> s1;
+        s1.push(root);
+        while (!s1.empty()) {
+            root = s1.top();
+            s1.pop();
+            res.push_back(root->val);
+            if (root->left)
+                s1.push(root->left);
+            if (root->right)
+                s1.push(root->right);
+        }
+        reverse(res.begin(), res.end());
+    }
+    vector<vector<int>> threeOrders(TreeNode* root) {
+        // write code here
+        vector<vector<int>> res;
+        res.resize(3);
+        prev(root, res[0]);
+        mid(root, res[1]);
+        post(root, res[2]);
+        return res;
+    }
+};
+
 int main() {
 
 	/*vector<int> arr({ 1,3,5,7,9,2,4,4,6,5,8,10 });
