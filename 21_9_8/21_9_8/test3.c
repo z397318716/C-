a59aa5108911c9981c260f58e1026ac25d0fabc0
z397318716 <397318716@qq.com>
2021-09-09 17:44:43 +0800

    练习

---
+++ 21_9_8/21_9_8/test3.c
@@ -0,0 +1,27 @@
+//#include<stdio.h>
+//int getlin(char* s) {
+//	char* p = s;
+//	while (*p != '\0') {
+//		p++;
+//	}
+//	return (p - s);
+//}
+//
+//
+//
+//
+//
+//int main() {
+//	/*int buf[] = { 3,5,4,1,2 };
+//	int max = 0;
+//	getmax(&max, buf, 5);
+//	printf("%d\n", max);*/
+//
+//	//int arr[3][3] = { 0 };
+//	//char b[30];
+//	//char* c = (char*)malloc(sizeof(char) * 10);
+//
+//	printf("%d,", getlin("geedluck!"));
+//
+//	return 0;
+//}
