5f3c83a7cb4a8c37c47b03d1a3705b677f197c4c
z397318716 <397318716@qq.com>
2021-09-14 13:05:12 +0800

    练习

---
+++ 21_9_14/21_9_14/test.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty())
+            return 0;
+        unordered_map<char, int> m1;
+        int res = 0;
+        int pos = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (m1.find(s[i]) == m1.end()) {
+                m1[s[i]]++;
+                res = max(res, (int)m1.size());
+            }
+            else {
+                while (pos < s.size() && m1.find(s[i]) != m1.end()) {
+                    m1.erase(s[pos++]);
+                }
+                m1[s[i]]++;
+                res = max(res, (int)m1.size());
+            }
+        }
+        return res;
+    }
+};
